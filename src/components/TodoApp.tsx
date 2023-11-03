3c5b8929b83d7aa1c4227a34a61683e0a72921f5
ryuji abe <ryuji-abe@Room335.local>
2023-11-04 04:28:11 +0900

    [Add] todo app

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoApp = () => {
+  return (
+    <section className="text-center mb-2">
+      <h1>Todo App</h1>
+      <form>
+        <input type="text" />
+        <button></button>
+      </form>
+    </section>
+  );
+};
+
+export default TodoApp;
